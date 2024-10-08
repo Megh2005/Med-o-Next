35f598d44d5902f42c76ee35da8aa1f3f51f8606
= <=>
2024-10-08 19:26:34 +0530

    frontend using next.js

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+import { LandingPageComponent } from "@/components/landing-page"; // Import the LandingPageComponent
+
+export default function Home() {
+  return (
+    <>
+      <LandingPageComponent />
+    </>
+  );
+}
